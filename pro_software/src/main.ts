c7a9f79b587995ea943ae4972edec133e3663a62
Daniela Sofia Alzate Franco <danielas.alzatef@uqvirtual.edu.co>
2024-11-12 15:03:54 -0500

    Crear diseño de solicitud de revisión

--- pro_software/src/main.ts
+++ pro_software/src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { LoginComponent } from './app/login/login.component';
+import { RequestReviewComponent } from './app/request-review/request-review.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(RequestReviewComponent, appConfig)
   .catch((err) => console.error(err));
