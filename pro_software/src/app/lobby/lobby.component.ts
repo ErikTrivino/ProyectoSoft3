fea2245c8463c724b824954c725e79087e9b173d
Daniela Sofia Alzate Franco <danielas.alzatef@uqvirtual.edu.co>
2024-11-12 13:53:38 -0500

    Crear diseño lobby

---
+++ pro_software/src/app/lobby/lobby.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lobby',
+  standalone: true,
+  imports: [],
+  templateUrl: './lobby.component.html',
+  styleUrl: './lobby.component.css'
+})
+export class LobbyComponent {
+
+
+}
---
+++ pro_software/src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  title = 'proyecto_software';
+
+  constructor(private router: Router) { }
+
+  onLogin() {
+
+    this.router.navigate(['/lobby']);
+  }
+}
