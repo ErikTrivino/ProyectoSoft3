712905dc321b7c4fb4bbf8d496afbd7866ba1f9f
Daniela Sofia Alzate Franco <danielas.alzatef@uqvirtual.edu.co>
2024-11-12 14:11:40 -0500

    Crear diseño solicitar revisión

---
+++ pro_software/src/app/request-review/request-review.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-review',
+  standalone: true,
+  imports: [],
+  templateUrl: './request-review.component.html',
+  styleUrl: './request-review.component.css'
+})
+export class RequestReviewComponent {
+
+}
