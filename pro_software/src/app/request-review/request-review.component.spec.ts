c394fb1bbb3261e0f1f5e2f6e0bdfce6bcebb7b4
Daniela Sofia Alzate Franco <danielas.alzatef@uqvirtual.edu.co>
2024-11-17 20:16:10 -0500

    Prueba unitaria review

--- pro_software/src/app/request-review/request-review.component.spec.ts
+++ pro_software/src/app/request-review/request-review.component.spec.ts
@@ -1,23 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { RequestReviewComponent } from './request-review.component';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { RequestReviewComponent } from './request-review.component'; 
+import { FormsModule } from '@angular/forms';
 
 describe('RequestReviewComponent', () => {
   let component: RequestReviewComponent;
   let fixture: ComponentFixture<RequestReviewComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RequestReviewComponent]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ RequestReviewComponent, FormsModule ]  
+    });
 
     fixture = TestBed.createComponent(RequestReviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should log selected date and time when clicking "Programar servicio"', fakeAsync(() => {
+    const btn = fixture.debugElement.nativeElement.querySelector('#agregarFechaHora');
+    btn.click(); // Abre el modal
+    
+    fixture.detectChanges();
+  
+    const dateInput = fixture.debugElement.nativeElement.querySelector('input[type="date"]');
+    const select = fixture.debugElement.nativeElement.querySelector('select');
+  
+
+    dateInput.value = '2024-11-17'; 
+    select.value = '10:00 AM'; 
+ 
+    dateInput.dispatchEvent(new Event('input'));
+    select.dispatchEvent(new Event('change')); 
+    fixture.detectChanges(); 
+    
+    const confirmBtn = fixture.debugElement.nativeElement.querySelector('.modal-actions .btn');
+    confirmBtn.click();
+    
+    fixture.detectChanges(); 
+    
+  
+    expect(component.selectedDate).toBe('2024-11-17');
+   
+  }));
+  
 });
