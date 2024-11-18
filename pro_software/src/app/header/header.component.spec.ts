f7bff33301fa76be65eca67f8c066700bd606f6a
Daniela Sofia Alzate Franco <danielas.alzatef@uqvirtual.edu.co>
2024-11-17 19:13:51 -0500

    Prueba unitaria header

--- pro_software/src/app/header/header.component.spec.ts
+++ pro_software/src/app/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,9 +9,8 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
-    })
-    .compileComponents();
+      imports: [HeaderComponent, RouterTestingModule], 
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
@@ -20,4 +20,16 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the correct title', () => {
+    const headerTitle = fixture.debugElement.query(By.css('h1')).nativeElement;
+    expect(headerTitle.textContent).toBe('Telesai');
+  });
+
+  it('should have three navigation links', () => {
+    const navLinks = fixture.debugElement.queryAll(By.css('nav ul li a'));
+    expect(navLinks.length).toBe(3);
+  });
+
+  
 });
